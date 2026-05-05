305854df9764f93710d196626d36b3f4a71108c7
Anakin Pregitzer <anakin@pregitzer.de>
2026-05-05 17:54:36 +0200

    Added missing dependencies and added loop to wait for the service to start

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 cd /var/www/html
 
-if [ ! -f wp-config.php ]; then
+until mariadb-admin ping -h mariadb -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" --silent 2>/dev/null; do
+    sleep 1
+done
+
+if [ ! -f wp-cli.phar ]; then
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
+fi
 
+if [ ! -f wp-config.php ]; then
     php84 -d memory_limit=-1 wp-cli.phar core download --allow-root
 
     php84 -d memory_limit=-1 wp-cli.phar config create \
@@ -13,7 +19,9 @@ if [ ! -f wp-config.php ]; then
         --dbpass=$MYSQL_PASSWORD \
         --dbhost=mariadb \
         --allow-root
+fi
 
+if ! php84 -d memory_limit=-1 wp-cli.phar core is-installed --allow-root 2>/dev/null; then
     php84 -d memory_limit=-1 wp-cli.phar core install \
         --url=$DOMAIN_NAME \
         --title=inception \
